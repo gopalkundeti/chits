b6138489c71297b3e4759e38b4c569ada89f3326
gopalkundeti <142778872+gopalkundeti@users.noreply.github.com>
2025-10-22 00:09:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+//import Button from 'react-bootstrap/Button';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Login from './pages/login';
+import Home from './pages/home';
+import Register from "./register";
+import Portfolio from "./portfolio";
+
+function Header() {
+  return (
+    <div className="App-header">
+      <ul className="nav nav-tabs">
+        <li class="nav-item"><Link to="/">Home</Link></li>
+        <li class="nav-item"><Link to="/login">Login</Link></li>
+      </ul>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+          <Route exact path="/portfolio" element={<Portfolio />}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
